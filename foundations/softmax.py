82cc81be2155a07f08f396b703122afd2348fb0e
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-06-19 20:44:51 +0800

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -5,5 +5,5 @@ from numpy.typing import NDArray
 class Solution:
 
     def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        total = np.sum(np.exp(z - z.max()))
-        return np.round(np.exp(z - z.max()) / total,4)
+        transformed = np.exp(z - z.max())
+        return np.round(transformed / np.sum(transformed), 4)
